531eda44c635b32980c1351ed67f157f975fc5af
joshua saavedra <joshuainegosaavedra@gmail.com>
2025-05-04 20:30:06 +0800

    fix: restore shoppage

---
+++ src/views/system/ShopPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppLayout from '@/components/layout/AppLayout.vue'
+import ProductsList from '@/components/common/products/ProductsList.vue'
+</script>
+
+<template>
+  <AppLayout>
+    <template #content>
+      <v-container>
+        <ProductsList />
+      </v-container>
+    </template>
+  </AppLayout>
+</template>
